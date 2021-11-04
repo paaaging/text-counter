f6be7a5fa3878f7409bbcdcf492a6fc605e9714a
paaaging <ikashakai@gmail.com>
2021-11-04 10:25:38 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card>
+          <v-card-title class="justify-center font-bold primary">
+            文字数チェッカー
+          </v-card-title>
+        </v-card>
+        <v-card class="logo py-4 d-flex justify-center">
+          <v-textarea
+            v-model="inputText"
+            name=""
+            label="入力欄"
+            value="文字数をカウントするテキストを入力して下さい。"
+            hint="こちらにテキストを入力して下さい。"
+          ></v-textarea>
+        </v-card>
+          <p class="char-box">文字数: <span>{{ textCount }}</span> </p>
+          <button @click="reset" class="btn-reset">リセット</button>
+      </v-col>
+    </v-row>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      inputText: ''
+    };
+  },
+  computed:{
+    textCount: function(){
+      var noSpace =　this.inputText.replace(/\s+/g, "");
+      return noSpace.length
+    },
+  },
+  methods:{
+  reset(){
+    this.inputText = ''
+  }
+}
+}
+</script>
